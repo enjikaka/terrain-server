c6bcd51105eb99dc99dbbfac397cfcba9f59544d
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2020-06-22 18:21:23 +0200

    Add OLC  dep. (Need to fork and fix issues to run)

--- app/deps.ts
+++ app/deps.ts
@@ -1,2 +1,3 @@
 export { DataTypes, Database, Model } from 'https://deno.land/x/denodb/mod.ts';
-export { serve, ServerRequest } from "https://deno.land/std/http/server.ts";
+export { serve, ServerRequest } from 'https://deno.land/std/http/server.ts';
+export { default as OpenLocationCode, CodeArea } from 'https://deno.land/x/gh:tspoke:typescript-open-location-code/src/open-location-code.ts';
